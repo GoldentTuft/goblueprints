9bf017cb9794c36cf83340cb3240a3bbb5e6e4e9
GoldentTuft <goldent@outlook.jp>
2020-08-05 07:02:35 +0900

    Read up to page 13

---
+++ chat/room.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type room struct {
+	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
+	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネルです。
+	join chan *client
+	// leaveはチャットルームから退出しようとしているクライアントのためのチャネルです。
+	leave chan *client
+	// clientsには在室しているすべてのクライアントが保持されます。
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退出
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// すべてのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
